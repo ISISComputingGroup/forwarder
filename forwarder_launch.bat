839ec92e0da6c1e9533ef770870751f0efec4de1
Jack Harper <harps912@gmail.com>
2025-07-31 10:28:26 +0100

    add forwarder_launch - still need to put in correct forwarder dir

---
+++ forwarder_launch.bat
@@ -0,0 +1,14 @@
+call C:\Instrument\dev\forwarder\.venv\scripts\activate
+
+set "EPICS_CA_ADDR_LIST=127.255.255.255 130.246.55.255"
+set "EPICS_CA_AUTO_ADDR_LIST=NO"
+
+
+if "%INSTRUMENT%" == "HIFI" (
+    set "BROKER=130.246.55.29:9092"
+) else (
+    set "BROKER=livedata.isis.cclrc.ac.uk:31092"
+)
+
+echo "starting forwarder"
+python C:\Instrument\dev\forwarder\forwarder_launch.py --status-topic=%BROKER%/%INSTRUMENT%_forwarderStatus --config-topic=%BROKER%/%INSTRUMENT%_forwarderConfig --storage-topic=%BROKER%/%INSTRUMENT%_forwarderStorage  --output-broker=%BROKER% --log-file C:\Instrument\var\log\forwarder\forwarder-log.txt
