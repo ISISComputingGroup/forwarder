ef0feaa139a4c936d7b0290bcb96731976a7bdaa
Jack Harper <jack.harper@stfc.ac.uk>
2025-07-31 13:42:31 +0100

    use forwarder in master

--- forwarder_launch.bat
+++ forwarder_launch.bat
@@ -1,4 +1,4 @@
-call C:\Instrument\Apps\EPICS\isis\forwarder\.venv\scripts\activate
+call C:\Instrument\Apps\EPICS\isis\forwarder\master\.venv\scripts\activate
 
 set "EPICS_CA_ADDR_LIST=127.255.255.255 130.246.55.255"
 set "EPICS_CA_AUTO_ADDR_LIST=NO"
@@ -10,4 +10,4 @@ if "%INSTRUMENT%" == "HIFI" (
 )
 
 echo "starting forwarder"
-python C:\Instrument\Apps\EPICS\isis\forwarder\forwarder_launch.py --status-topic=%BROKER%/%INSTRUMENT%_forwarderStatus --config-topic=%BROKER%/%INSTRUMENT%_forwarderConfig --storage-topic=%BROKER%/%INSTRUMENT%_forwarderStorage  --output-broker=%BROKER%
+python C:\Instrument\Apps\EPICS\isis\forwarder\master\forwarder_launch.py --status-topic=%BROKER%/%INSTRUMENT%_forwarderStatus --config-topic=%BROKER%/%INSTRUMENT%_forwarderConfig --storage-topic=%BROKER%/%INSTRUMENT%_forwarderStorage  --output-broker=%BROKER%
