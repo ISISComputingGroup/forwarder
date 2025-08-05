da023d319092f8d53302332e53dc75b268e63d92
Jack Harper <jack.harper@stfc.ac.uk>
2025-08-05 11:03:45 +0100

    add 30s status interval

--- forwarder_launch.bat
+++ forwarder_launch.bat
@@ -13,5 +13,5 @@ set "EPICS_CA_ADDR_LIST=127.255.255.255 130.246.55.255"
 set "EPICS_CA_AUTO_ADDR_LIST=NO"
 
 echo "starting forwarder"
-python %~dp0forwarder_launch.py --status-topic=%KAFKA_BROKER%/%INSTRUMENT%_forwarderStatus --config-topic=%KAFKA_BROKER%/%INSTRUMENT%_forwarderConfig --storage-topic=%KAFKA_BROKER%/%INSTRUMENT%_forwarderStorage  --output-broker=%KAFKA_BROKER%
+python %~dp0forwarder_launch.py --status-topic=%KAFKA_BROKER%/%INSTRUMENT%_forwarderStatus --config-topic=%KAFKA_BROKER%/%INSTRUMENT%_forwarderConfig --storage-topic=%KAFKA_BROKER%/%INSTRUMENT%_forwarderStorage  --output-broker=%KAFKA_BROKER%  --status-interval=30000
 
